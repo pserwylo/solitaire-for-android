71d2ee7f6a10b69629154223459c27dcac8e120c
Peter Serwylo <peter@ivt.com.au>
2013-04-16 21:31:19 +1000

    Reformated code to a style I'm a bit more comfortable with.

--- src/com/kmagic/solitaire/Replay.java
+++ src/com/kmagic/solitaire/Replay.java
@@ -12,93 +12,99 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/ 
+*/
 package com.kmagic.solitaire;
 
-import android.util.Log;
-import java.util.Stack;
+import android.util.*;
+
+import java.util.*;
 
 public class Replay implements Runnable {
-  private Stack<Move> mMoveStack;
-  private SolitaireView mView;
-  private AnimateCard mAnimateCard;
-  private CardAnchor[] mCardAnchor;
-  private boolean mIsPlaying;
+	private Stack<Move> mMoveStack;
+	private SolitaireView mView;
+	private AnimateCard mAnimateCard;
+	private CardAnchor[] mCardAnchor;
+	private boolean mIsPlaying;
 
-  private Card[] mSinkCard;
-  private int mSinkCount;
-  private int mEventCount;
-  private CardAnchor mSinkAnchor;
-  private CardAnchor mSinkFrom;
-  private boolean mSinkUnhide;
+	private Card[] mSinkCard;
+	private int mSinkCount;
+	private int mEventCount;
+	private CardAnchor mSinkAnchor;
+	private CardAnchor mSinkFrom;
+	private boolean mSinkUnhide;
 
-  public Replay(SolitaireView view, AnimateCard animateCard) {
-    mView = view;
-    mAnimateCard = animateCard;
-    mIsPlaying = false;
-    mMoveStack = new Stack<Move>();
-    mSinkCard = new Card[104];
-  }
+	public Replay( SolitaireView view, AnimateCard animateCard ) {
+		mView = view;
+		mAnimateCard = animateCard;
+		mIsPlaying = false;
+		mMoveStack = new Stack<Move>();
+		mSinkCard = new Card[104];
+	}
 
-  public boolean IsPlaying() { return mIsPlaying; }
-  public void StopPlaying() { mIsPlaying = false; }
+	public boolean IsPlaying() {
+		return mIsPlaying;
+	}
 
-  public void StartReplay(Stack<Move> history, CardAnchor[] anchor) {
-    mCardAnchor = anchor;
-    mMoveStack.clear();
-    while (!history.empty()) {
-      Move move = history.peek();
-      if (move.GetToBegin() != move.GetToEnd()) {
-        for (int i = move.GetToEnd(); i >= move.GetToBegin(); i--) {
-          mMoveStack.push(new Move(move.GetFrom(), i, 1, false, false));
-        }
-      } else {
-        mMoveStack.push(move);
-      }
-      mView.Undo();
-    }
-    mView.DrawBoard();
-    mIsPlaying = true;
-    PlayNext();
-  }
+	public void StopPlaying() {
+		mIsPlaying = false;
+	}
 
-  public void PlayNext() {
-    if (!mIsPlaying || mMoveStack.empty()) {
-      mIsPlaying = false;
-      mView.StopAnimating();
-      return;
-    }
-    Move move = mMoveStack.pop();
+	public void StartReplay( Stack<Move> history, CardAnchor[] anchor ) {
+		mCardAnchor = anchor;
+		mMoveStack.clear();
+		while ( !history.empty() ) {
+			Move move = history.peek();
+			if ( move.GetToBegin() != move.GetToEnd() ) {
+				for ( int i = move.GetToEnd(); i >= move.GetToBegin(); i-- ) {
+					mMoveStack.push( new Move( move.GetFrom(), i, 1, false, false ) );
+				}
+			} else {
+				mMoveStack.push( move );
+			}
+			mView.Undo();
+		}
+		mView.DrawBoard();
+		mIsPlaying = true;
+		PlayNext();
+	}
 
-    if (move.GetToBegin() == move.GetToEnd()) {
-      mSinkCount = move.GetCount();
-      mEventCount = 0;
-      mSinkAnchor = mCardAnchor[move.GetToBegin()];
-      mSinkUnhide = move.GetUnhide();
-      mSinkFrom = mCardAnchor[move.GetFrom()];
+	public void PlayNext() {
+		if ( !mIsPlaying || mMoveStack.empty() ) {
+			mIsPlaying = false;
+			mView.StopAnimating();
+			return;
+		}
+		Move move = mMoveStack.pop();
 
-      if (move.GetInvert()) {
-        for (int i = 0; i < mSinkCount; i++) {
-          mSinkCard[i] = mSinkFrom.PopCard();
-        }
-      } else {
-        for (int i = mSinkCount-1; i >= 0; i--) {
-          mSinkCard[i] = mSinkFrom.PopCard();
-        }
-      }
-      mAnimateCard.MoveCards(mSinkCard, mSinkAnchor, mSinkCount, this);
-    } else {
-      Log.e("Replay.java", "Invalid move encountered, aborting.");
-      mIsPlaying = false;
-    }
-  }
+		if ( move.GetToBegin() == move.GetToEnd() ) {
+			mSinkCount = move.GetCount();
+			mEventCount = 0;
+			mSinkAnchor = mCardAnchor[move.GetToBegin()];
+			mSinkUnhide = move.GetUnhide();
+			mSinkFrom = mCardAnchor[move.GetFrom()];
 
-  public void run() {
-    if (mIsPlaying) {
-      if (mSinkUnhide) {
-        mSinkFrom.UnhideTopCard();
-      }
-      PlayNext();
-    }
-  }
+			if ( move.GetInvert() ) {
+				for ( int i = 0; i < mSinkCount; i++ ) {
+					mSinkCard[i] = mSinkFrom.PopCard();
+				}
+			} else {
+				for ( int i = mSinkCount - 1; i >= 0; i-- ) {
+					mSinkCard[i] = mSinkFrom.PopCard();
+				}
+			}
+			mAnimateCard.MoveCards( mSinkCard, mSinkAnchor, mSinkCount, this );
+		} else {
+			Log.e( "Replay.java", "Invalid move encountered, aborting." );
+			mIsPlaying = false;
+		}
+	}
+
+	public void run() {
+		if ( mIsPlaying ) {
+			if ( mSinkUnhide ) {
+				mSinkFrom.UnhideTopCard();
+			}
+			PlayNext();
+		}
+	}
 }
--- src/com/kmagic/solitaire/Solitaire.java
+++ src/com/kmagic/solitaire/Solitaire.java
@@ -12,201 +12,198 @@
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-*/ 
+*/
 package com.kmagic.solitaire;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Window;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SubMenu;
-import android.view.View;
-import android.widget.TextView;
+import android.app.*;
+import android.content.*;
+import android.content.pm.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 // Base activity class.
 public class Solitaire extends Activity {
-  private static final int MENU_NEW_GAME  = 1;
-  private static final int MENU_RESTART   = 2;
-  private static final int MENU_OPTIONS   = 3;
-  private static final int MENU_SAVE_QUIT = 4;
-  private static final int MENU_QUIT      = 5;
-  private static final int MENU_SOLITAIRE = 6;
-  private static final int MENU_SPIDER    = 7;
-  private static final int MENU_FREECELL  = 8;
-  private static final int MENU_FORTYTHIEVES = 9;
-  private static final int MENU_STATS     = 10;
-  private static final int MENU_HELP      = 11;
+	private static final int MENU_NEW_GAME = 1;
+	private static final int MENU_RESTART = 2;
+	private static final int MENU_OPTIONS = 3;
+	private static final int MENU_SAVE_QUIT = 4;
+	private static final int MENU_QUIT = 5;
+	private static final int MENU_SOLITAIRE = 6;
+	private static final int MENU_SPIDER = 7;
+	private static final int MENU_FREECELL = 8;
+	private static final int MENU_FORTYTHIEVES = 9;
+	private static final int MENU_STATS = 10;
+	private static final int MENU_HELP = 11;
 
-  // View extracted from main.xml.
-  private View mMainView;
-  private SolitaireView mSolitaireView;
-  private SharedPreferences mSettings;
+	// View extracted from main.xml.
+	private View mMainView;
+	private SolitaireView mSolitaireView;
+	private SharedPreferences mSettings;
 
-  private boolean mDoSave;
-  
-  // Shared preferences are where the various user settings are stored.
-  public SharedPreferences GetSettings() { return mSettings; }
+	private boolean mDoSave;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    mDoSave = true;
+	// Shared preferences are where the various user settings are stored.
+	public SharedPreferences GetSettings() {
+		return mSettings;
+	}
 
-    // Force landscape and no title for extra room
-    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		mDoSave = true;
 
-    // If the user has never accepted the EULA show it again.
-    mSettings = getSharedPreferences("SolitairePreferences", 0);
-    setContentView(R.layout.main);
-    mMainView = findViewById(R.id.main_view);
-    mSolitaireView = (SolitaireView) findViewById(R.id.solitaire);
-    mSolitaireView.SetTextView((TextView) findViewById(R.id.text));
+		// Force landscape and no title for extra room
+		setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
+		requestWindowFeature( Window.FEATURE_NO_TITLE );
 
-    //StartSolitaire(savedInstanceState);
-  }
+		// If the user has never accepted the EULA show it again.
+		mSettings = getSharedPreferences( "SolitairePreferences", 0 );
+		setContentView( R.layout.main );
+		mMainView = findViewById( R.id.main_view );
+		mSolitaireView = (SolitaireView) findViewById( R.id.solitaire );
+		mSolitaireView.SetTextView( (TextView) findViewById( R.id.text ) );
 
-  // Entry point for starting the game.
-  //public void StartSolitaire(Bundle savedInstanceState) {
-  @Override
-  public void onStart() {
-    super.onStart();
-    if (mSettings.getBoolean("SolitaireSaveValid", false)) {
-      SharedPreferences.Editor editor = GetSettings().edit();
-      editor.putBoolean("SolitaireSaveValid", false);
-      editor.commit();
-      // If save is corrupt, just start a new game.
-      if (mSolitaireView.LoadSave()) {
-        HelpSplashScreen();
-        return;
-      }
-    }
+		//StartSolitaire(savedInstanceState);
+	}
 
-    mSolitaireView.InitGame(mSettings.getInt("LastType", Rules.SOLITAIRE));
-    HelpSplashScreen();
-  }
+	// Entry point for starting the game.
+	//public void StartSolitaire(Bundle savedInstanceState) {
+	@Override
+	public void onStart() {
+		super.onStart();
+		if ( mSettings.getBoolean( "SolitaireSaveValid", false ) ) {
+			SharedPreferences.Editor editor = GetSettings().edit();
+			editor.putBoolean( "SolitaireSaveValid", false );
+			editor.commit();
+			// If save is corrupt, just start a new game.
+			if ( mSolitaireView.LoadSave() ) {
+				HelpSplashScreen();
+				return;
+			}
+		}
 
-  // Force show the help if this is the first time played. Sadly no one reads
-  // it anyways.
-  private void HelpSplashScreen() {
-    if (!mSettings.getBoolean("PlayedBefore", false)) {
-      mSolitaireView.DisplayHelp();
-    }
-  }
+		mSolitaireView.InitGame( mSettings.getInt( "LastType", Rules.SOLITAIRE ) );
+		HelpSplashScreen();
+	}
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    super.onCreateOptionsMenu(menu);
+	// Force show the help if this is the first time played. Sadly no one reads
+	// it anyways.
+	private void HelpSplashScreen() {
+		if ( !mSettings.getBoolean( "PlayedBefore", false ) ) {
+			mSolitaireView.DisplayHelp();
+		}
+	}
 
-    SubMenu subMenu = menu.addSubMenu(0, MENU_NEW_GAME, 0, R.string.menu_newgame);
-    subMenu.add(0, MENU_SOLITAIRE, 0, R.string.menu_solitaire);
-    subMenu.add(0, MENU_SPIDER, 0, R.string.menu_spider);
-    subMenu.add(0, MENU_FREECELL, 0, R.string.menu_freecell);
-    subMenu.add(0, MENU_FORTYTHIEVES, 0, R.string.menu_fortythieves);
+	@Override
+	public boolean onCreateOptionsMenu( Menu menu ) {
+		super.onCreateOptionsMenu( menu );
 
-    menu.add(0, MENU_RESTART, 0, R.string.menu_restart);
-    menu.add(0, MENU_OPTIONS, 0, R.string.menu_options);
-    menu.add(0, MENU_SAVE_QUIT, 0, R.string.menu_save_quit);
-    menu.add(0, MENU_QUIT, 0, R.string.menu_quit);
-    menu.add(0, MENU_STATS, 0, R.string.menu_stats);
-    menu.add(0, MENU_HELP, 0, R.string.menu_help);
-    return true;
-  }
+		SubMenu subMenu = menu.addSubMenu( 0, MENU_NEW_GAME, 0, R.string.menu_newgame );
+		subMenu.add( 0, MENU_SOLITAIRE, 0, R.string.menu_solitaire );
+		subMenu.add( 0, MENU_SPIDER, 0, R.string.menu_spider );
+		subMenu.add( 0, MENU_FREECELL, 0, R.string.menu_freecell );
+		subMenu.add( 0, MENU_FORTYTHIEVES, 0, R.string.menu_fortythieves );
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    switch (item.getItemId()) {
-      case MENU_SOLITAIRE:
-        mSolitaireView.InitGame(Rules.SOLITAIRE);
-        break;
-      case MENU_SPIDER:
-        mSolitaireView.InitGame(Rules.SPIDER);
-        break;
-      case MENU_FREECELL:
-        mSolitaireView.InitGame(Rules.FREECELL);
-        break;
-      case MENU_FORTYTHIEVES:
-        mSolitaireView.InitGame(Rules.FORTYTHIEVES);
-        break;
-      case MENU_RESTART:
-        mSolitaireView.RestartGame();
-        break;
-      case MENU_STATS:
-        DisplayStats();
-        break;
-      case MENU_OPTIONS:
-        DisplayOptions();
-        break;
-      case MENU_HELP:
-        mSolitaireView.DisplayHelp();
-        break;
-      case MENU_SAVE_QUIT:
-        mSolitaireView.SaveGame();
-        mDoSave = false;
-        finish();
-        break;
-      case MENU_QUIT:
-        mDoSave = false;
-        finish();
-        break;
-    }
+		menu.add( 0, MENU_RESTART, 0, R.string.menu_restart );
+		menu.add( 0, MENU_OPTIONS, 0, R.string.menu_options );
+		menu.add( 0, MENU_SAVE_QUIT, 0, R.string.menu_save_quit );
+		menu.add( 0, MENU_QUIT, 0, R.string.menu_quit );
+		menu.add( 0, MENU_STATS, 0, R.string.menu_stats );
+		menu.add( 0, MENU_HELP, 0, R.string.menu_help );
+		return true;
+	}
 
-    return false;
-  }
+	@Override
+	public boolean onOptionsItemSelected( MenuItem item ) {
+		switch ( item.getItemId() ) {
+			case MENU_SOLITAIRE:
+				mSolitaireView.InitGame( Rules.SOLITAIRE );
+				break;
+			case MENU_SPIDER:
+				mSolitaireView.InitGame( Rules.SPIDER );
+				break;
+			case MENU_FREECELL:
+				mSolitaireView.InitGame( Rules.FREECELL );
+				break;
+			case MENU_FORTYTHIEVES:
+				mSolitaireView.InitGame( Rules.FORTYTHIEVES );
+				break;
+			case MENU_RESTART:
+				mSolitaireView.RestartGame();
+				break;
+			case MENU_STATS:
+				DisplayStats();
+				break;
+			case MENU_OPTIONS:
+				DisplayOptions();
+				break;
+			case MENU_HELP:
+				mSolitaireView.DisplayHelp();
+				break;
+			case MENU_SAVE_QUIT:
+				mSolitaireView.SaveGame();
+				mDoSave = false;
+				finish();
+				break;
+			case MENU_QUIT:
+				mDoSave = false;
+				finish();
+				break;
+		}
 
-  @Override
-  protected void onPause() {
-    super.onPause();
-    mSolitaireView.onPause();
-  }
+		return false;
+	}
 
-  @Override
-  protected void onStop() {
-    super.onStop();
-    if (mDoSave) {
-      mSolitaireView.SaveGame();
-    }
-  }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSolitaireView.onPause();
+	}
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    mSolitaireView.onResume();
-  }
+	@Override
+	protected void onStop() {
+		super.onStop();
+		if ( mDoSave ) {
+			mSolitaireView.SaveGame();
+		}
+	}
 
-  @Override
-  public void onSaveInstanceState(Bundle outState) {
-    super.onSaveInstanceState(outState);
-  }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSolitaireView.onResume();
+	}
 
-  public void DisplayOptions() {
-    mSolitaireView.SetTimePassing(false);
-    new Options(this, mSolitaireView.GetDrawMaster());
-  }
+	@Override
+	public void onSaveInstanceState( Bundle outState ) {
+		super.onSaveInstanceState( outState );
+	}
 
-  public void DisplayStats() {
-    mSolitaireView.SetTimePassing(false);
-    new Stats(this, mSolitaireView);
-  }
+	public void DisplayOptions() {
+		mSolitaireView.SetTimePassing( false );
+		new Options( this, mSolitaireView.GetDrawMaster() );
+	}
 
-  public void CancelOptions() {
-    setContentView(mMainView);
-    mSolitaireView.requestFocus();
-    mSolitaireView.SetTimePassing(true);
-  }
+	public void DisplayStats() {
+		mSolitaireView.SetTimePassing( false );
+		new Stats( this, mSolitaireView );
+	}
 
-  public void NewOptions() {
-    setContentView(mMainView);
-    mSolitaireView.InitGame(mSettings.getInt("LastType", Rules.SOLITAIRE));
-  }
+	public void CancelOptions() {
+		setContentView( mMainView );
+		mSolitaireView.requestFocus();
+		mSolitaireView.SetTimePassing( true );
+	}
 
-  // This is called for option changes that require a refresh, but not a new game
-  public void RefreshOptions() {
-    setContentView(mMainView);
-    mSolitaireView.RefreshOptions();
-  }
+	public void NewOptions() {
+		setContentView( mMainView );
+		mSolitaireView.InitGame( mSettings.getInt( "LastType", Rules.SOLITAIRE ) );
+	}
+
+	// This is called for option changes that require a refresh, but not a new game
+	public void RefreshOptions() {
+		setContentView( mMainView );
+		mSolitaireView.RefreshOptions();
+	}
 }
